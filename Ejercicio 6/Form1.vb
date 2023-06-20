8f4bdf0c374885a7ebf5fa9a841d25302169961a
Bruno Casenave <brunoorope@hotmail.com>
2023-06-19 21:09:30 -0400

    Ejercicios

---
+++ Ejercicio 6/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Palabra As String
+        Dim PrimerLetra As String
+        Dim PrimeraLetra As Integer
+        Dim UltimaLetra As String
+        Dim Contador As Integer
+        Dim Largo As Integer
+        Dim Pl As Integer
+        Dim A As Integer
+        Pl = 1
+        A = 1
+        PrimeraLetra = 1
+
+        Palabra = InputBox("Ingrese una palabra")
+        Largo = Len(Palabra)
+        Do
+            UltimaLetra = Mid(Palabra, Largo, Largo)
+            PrimerLetra = Mid(Palabra, Pl, Pl)
+
+            If PrimerLetra = UltimaLetra Then
+                Contador = Contador + 1
+            End If
+            A = A + 1
+            Largo = Largo + 1
+        Loop Until (PrimeraLetra < Largo)
+        If Contador = 0 Then
+            Label1.Text = "La palabra no es palindroma"
+        Else
+            Label1.Text = "La palabra es palindroma"
+        End If
+
+    End Sub
+End Class
---
+++ Ejercicio 8/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Frase As String
+        Dim Largo As Integer
+        Dim Letras As String
+        Dim A As Integer
+        Dim Contador As Integer
+        A = 1
+        Frase = InputBox("Escribir una frase")
+        Largo = Frase.Length
+        Do
+            Letras = Frase.Substring(A - 1, 1)
+            If Letras = "," Then
+                Contador = Contador + 1
+            End If
+            A = A + 1
+        Loop Until A = Largo
+        Label1.Text = "La cantidad de comas es:" & Contador
+    End Sub
+End Class
